cc0dccb21caa4a21839a488a7374ec6bb424224b
sarye.haddadi <sarye@live.fr>
2022-01-09 18:25:04 +0100

    65. Client side validations

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -6,7 +6,11 @@ public class Category
 {
 	[Key]
 	public int Id { get; set; }
+
 	[Required]
 	public string Name { get; set; }
+
+	[Display(Name="Display Order")]
+	[Range(1, 100, ErrorMessage = "Display order must be between 1 and 100!!")]
 	public int DisplayOrder { get; set; }
 }
--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,39 @@
+using AbbyWeb.Data;
+using AbbyWeb.Pages.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories;
 
+[BindProperties]
 public class CreateModel : PageModel
 {
-    public void OnGet()
+	private readonly ApplicationDbContext _db;
+	public Category Category { get; set; }
+
+	public CreateModel(ApplicationDbContext db)
+	{
+		_db = db;
+	}
+
+	public void OnGet()
     {
     }
+
+	public async Task<IActionResult> OnPost()
+	{
+		if(Category.Name == Category.DisplayOrder.ToString())
+		{
+			ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name."); ;
+		}
+		if(ModelState.IsValid)
+		{
+			await _db.Category.AddAsync(Category);
+			await _db.SaveChangesAsync();
+			return RedirectToPage("Index");
+		}
+
+		return Page();
+	}
 }
 
